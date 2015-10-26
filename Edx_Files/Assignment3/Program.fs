9c8ca4c4928612e1140156f3a4da977639f59840
Tom Chippendale <TChippendale@yahoo.com>
2015-10-25 19:54:20 -0700

    Completed Assignments

---
+++ Edx_Files/Assignment3/Program.fs
@@ -0,0 +1,36 @@
+﻿open System
+
+let goldenRatio = (1.0 + Math.Sqrt(5.0))/2.0
+let calculateRatio x = x*goldenRatio
+let isNumber x = 
+    let mutable valid = true
+    for c in x do
+        if (Char.IsDigit(c) = false) then
+           valid <- false
+    valid
+
+[<EntryPoint>]
+let main argv = 
+
+    let ratios =
+        [ let mutable run = true
+          while run do
+            
+            Console.WriteLine(":Enter value: ")
+            let value = Console.ReadLine()
+            if isNumber value then
+                yield (float value, calculateRatio(float value))
+            else
+                Console.WriteLine("Invalid Number!")
+            Console.WriteLine("Do your want to add another value (y/n)?")
+            if Console.ReadLine().ToLower() <> "y" then
+                run <- false]
+
+
+    Console.WriteLine("\nResults:")
+    for x in ratios do
+        let v,r = x
+        Console.WriteLine("Value: {0:0.00}     Result: {1:0.00}", v, r)
+
+    Console.ReadLine()
+    0 // return an integer exit code
---
+++ Edx_Files/Assignment4/Program.fs
@@ -0,0 +1,63 @@
+﻿open System
+open System.IO
+
+type Target = 
+    {
+        X : float
+        Y : float 
+        Speed : float
+        Distance : float
+        Name : string
+    }
+
+
+(* Helper Functions*)
+let angle_of_reach distance speed = 0.5*Math.Asin((9.81*distance)/(speed*speed))
+let angle x y = Math.Atan2(y,x)
+let distance_travelled speed angle = (speed*speed)*Math.Sin(2.0*angle)/9.81
+
+(* Helper Function to get the file name*)
+let GetFile = 
+    Console.Write("Enter the full path to the name of the input file: ")
+    Console.ReadLine()
+
+[<EntryPoint>]
+let main argv = 
+
+    try
+        (* Read the data from the file *)
+        use input =
+            new StreamReader(match argv.Length with
+                             | 0 -> GetFile    
+                             | _ -> argv.[0])
+
+        let targets =
+            [ while not input.EndOfStream do
+                  let raw = input.ReadLine()
+                  let values = raw.Split(',')
+                  yield { X = float values.[0]
+                          Y = float values.[1]
+                          Speed = float values.[2]
+                          Distance = float values.[3]
+                          Name = values.[4] } ]
+
+        (* Loop through all the targets and determine if it hit the target *)
+        for t in targets do
+            match t with
+            | { Speed=s; Distance=d; Name=n} when (9.81*d)/(s*s) > 1.0 -> Console.WriteLine("Gun {0} Target Unreachable",n)
+            | { X=x; Y=y; Speed=s; Distance=d; Name=n } when 
+                (distance_travelled s (angle x y))<>d -> 
+                Console.WriteLine("Gun {0} Missed - Change Angle By: {1:0.00}", n, (angle_of_reach d s) - (angle x y))
+            | { X=x; Y=y; Speed=s; Distance=d; Name=n} when (distance_travelled s (angle x y))=d -> Console.WriteLine("Gun {0} Hit", n)
+            | _ -> Console.WriteLine("Error!!!")
+        Console.ReadLine()
+        0
+    with
+    | :? System.IO.FileNotFoundException ->
+        Console.Write("File Not Found. Press a key to exit")
+        Console.ReadKey()
+        -1
+    | _ ->
+        Console.Write("Something else happened")
+        Console.ReadKey()
+        -1
